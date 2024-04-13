a3e0cfa98bd9cb9ab876977cd6f4348d52d0b3be
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-04-13 07:17:38 +0300

    Authenticating users

--- server/app.py
+++ server/app.py
@@ -71,10 +71,10 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
-api.add_resource(CheckSession, '/check_session')
-api.add_resource(Logout, '/logout')
-api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
